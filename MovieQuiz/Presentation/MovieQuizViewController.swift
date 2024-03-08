f9f863fbabf4f9d001f29b51eb7c38357690e78d
Alexander Balyanov <alex44298@yandex.ru>
2024-03-08 21:07:28 +0500

    закончил работу над приложением

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -5,20 +5,12 @@ final class MovieQuizViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let currentQuestion = questions[currentQuestionIndex]
+        let firstQuestion = questions[currentQuestionIndex]
         
-//        let convertToViewModel = convert(model: QuizQuestion(
-//            image: currentQuestion.image,
-//            text: currentQuestion.text,
-//            correctAnswer: currentQuestion.correctAnswer))
-//        
-//        show(quiz: QuizStepViewModel(
-//            image: convertToViewModel.image,
-//            question: convertToViewModel.question,
-//            questionNumber: convertToViewModel.questionNumber))
-        
-        let viewModel = convert(model: currentQuestion)
+        let viewModel = convert(model: firstQuestion)
         show(quiz: viewModel)
+        
+        setupImageView()
     }
     
     private var currentQuestionIndex = 0
@@ -88,6 +80,12 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var imageView: UIImageView!
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
+    @IBOutlet private var yesButtonState: UIButton!
+    @IBOutlet private var noButtonState: UIButton!
+    
+    private func setupImageView() {
+        imageView.layer.cornerRadius = 20
+    }
     
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
        let questionStep = QuizStepViewModel(
@@ -108,14 +106,28 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderWidth = 8
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         
+        if isCorrect == true {
+            correctAnswers += 1
+        }
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
             self.showNextQuestionOrResults()
         }
     }
     
     private func showNextQuestionOrResults() {
+        imageView.layer.borderColor = UIColor.white.withAlphaComponent(0.0).cgColor
+        
+        yesButtonState.isEnabled = true
+        noButtonState.isEnabled = true
+        
         if currentQuestionIndex == questions.count - 1 {
+            let viewModel = QuizResultsViewModel(
+                title: "Этот раунд окончен!",
+                text: "ваш результат \(correctAnswers)/\(questions.count)",
+                buttonText: "Сыграть еще раз")
             
+                showAlert(quiz: viewModel)
         } else {
             currentQuestionIndex += 1
             
@@ -126,12 +138,33 @@ final class MovieQuizViewController: UIViewController {
         }
     }
     
+    private func showAlert(quiz result: QuizResultsViewModel) {
+        let alert = UIAlertController(
+            title: result.title,
+            message: result.text,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            self.currentQuestionIndex = 0
+            self.correctAnswers = 0
+            
+            let firstQuestion = self.questions[self.currentQuestionIndex]
+            let viewModel = self.convert(model: firstQuestion)
+            self.show(quiz: viewModel)
+        }
+        
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
+    }
     
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         let currentQuestion = questions[currentQuestionIndex]
         let givenAnswer = true
         
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        yesButtonState.isEnabled = false
+        noButtonState.isEnabled = false
     }
     
     @IBAction private func noButtonClicked(_ sender: UIButton) {
@@ -139,8 +172,10 @@ final class MovieQuizViewController: UIViewController {
         let givenAnswer = false
         
         showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        
+        yesButtonState.isEnabled = false
+        noButtonState.isEnabled = false
     }
-    
 }
 
 /*
