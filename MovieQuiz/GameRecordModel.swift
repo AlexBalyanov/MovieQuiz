3f158406d2bc9b342602fcabd5558b279f26bf6d
Alexander Balyanov <alex44298@yandex.ru>
2024-04-07 14:03:40 +0500

    перед выполением финальной задачи 5 спринта

---
+++ MovieQuiz/GameRecordModel.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+struct GameRecord: Codable {
+    let correct: Int
+    let total: Int
+    let date: Date
+    
+    private func isBetterThan(_ another: GameRecord) -> Bool {
+        correct > another.correct
+    }
+}
---
+++ MovieQuiz/StatisticServiceImplementation.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+final class StatisticServiceImplementation: StatisticService {
+    
+    private let userDefaults = UserDefaults.standard
+    
+    var totalAccuracy: Double
+    
+    var gamesCount: Int
+    
+    var bestGame: GameRecord {
+        get {
+            guard let data = userDefaults.data(forKey: Keys.bestGame.rawValue),
+            let record = try? JSONDecoder().decode(GameRecord.self, from: data) else {
+                return .init(correct: 0, total: 0, date: Date())
+            }
+            return record
+        }
+        
+        set {
+            guard let data = try? JSONEncoder().encode(newValue) else {
+                print ("Невозможно сохранить результат.")
+                return
+            }
+            userDefaults.setValue(data, forKey: Keys.bestGame.rawValue)
+        }
+    }
+    
+    private enum Keys: String {
+        case correct, total, bestGame, gamesCount
+    }
+    
+    func store(correct count: Int, total amount: Int) {
+        <#code#>
+    }
+    
+    
+}
